d1f1662799ad08cd53d288041513aa5c1611ccf7
sanctus patricius <patriciussanctus@gmail.com>
2020-07-14 12:14:02 +0400

    random grids with tea and chill lol

--- src/app.vue
+++ src/app.vue
@@ -1,13 +1,17 @@
 <template lang="pug">
   #app
     app-header
+    random-breed#first-breed(v-if="loaded", :data="allBreeds[firstBreedKey]", :name="firstBreedKey")
     #main-breeds
-      random-breed(v-for="(breed, name) in allBreeds", :data="breed", :name="name")
+      random-breed(v-for="(breed, name, index) in allBreeds", :data="breed", :key="index", :name="name")
 </template>
 
 <script>
   import appHeader from './app-header'
   import randomBreed from './ui/random-breed'
+  import { shuffle } from 'lodash-es'
+
+  window.shuffle = shuffle
 
   export default {
     name: 'App',
@@ -16,15 +20,26 @@
       randomBreed
     },
     data: () => ({
-      allBreeds: []
+      allBreeds: [],
+      loaded: false
     }),
+    computed: {
+      firstBreedKey: ({ allBreeds }) => allBreeds && Object.keys(allBreeds)[0]
+    },
     created () {
       this.$http.get('breeds/list/all').then(({ data }) => {
         if (!data || !data.message) {
           this.allBreeds = new Error('failed "breeds/list/all" API lol =/')
           return
         }
-        this.allBreeds = data.message
+
+        const breedsKeys = Object.keys(data.message)
+
+        // this.allBreeds = shuffle(data.message)
+        this.allBreeds = shuffle(breedsKeys).reduce((acc, cur) => {
+          return Object.assign(acc, { [cur]: data.message[cur] })
+        }, {})
+        this.loaded = true
       })
     }
   }
@@ -32,8 +47,14 @@
 
 <style lang="sass">
   body
+    padding: 0
+    margin: 0
     background: linear-gradient(180deg, #1B1A1F 0%, #111013 99.09%)
+    *
+      box-sizing: border-box
   #app
+    width: 100vw
+    padding: 30px 60px
     font-family: Avenir, Helvetica, Arial, sans-serif
     -webkit-font-smoothing: antialiased
     -moz-osx-font-smoothing: grayscale
@@ -41,7 +62,19 @@
     // color: #2c3e50
     margin-top: 100px
     color: #626262
+    // margin: 30px 60px
+    // max-width: 100vw
     a[href]
       color: #626262
       text-decoration: none
+    #first-breed
+      height: 513px
+      margin-bottom: 30px
+    #main-breeds
+      display: grid
+      grid-template-rows: 290px
+      grid-template-columns: repeat(3, 1fr)
+      grid-row-gap: 10px
+      grid-column-gap: 10px
+      grid-gap: 30px
 </style>
--- src/ui/random-breed.vue
+++ src/ui/random-breed.vue
@@ -1,7 +1,11 @@
 <template lang="pug">
   .random-breed
     img(:src="imgUrl")
+    .resize
+    .inner
     p.name {{ name }}
+    svg.vector-like(width="29" height="26" viewBox="0 0 29 26" fill="none" xmlns="http://www.w3.org/2000/svg")
+      path(fill-rule="evenodd" clip-rule="evenodd" d="M13.7579 25.6993L2.62464 14.8743C-0.874882 11.4716 -0.874882 5.9453 2.62464 2.54267C5.87363 -0.61635 10.9956 -0.831789 14.5 1.89636C18.0044 -0.831789 23.1264 -0.61635 26.3754 2.54267C29.8749 5.9453 29.8749 11.4716 26.3754 14.8743L15.2421 25.6993C14.8297 26.1003 14.1703 26.1003 13.7579 25.6993ZM4.10909 4.04943C1.45989 6.62528 1.45989 10.7919 4.10909 13.3678L14.5002 23.4712L24.8914 13.3678C27.5406 10.7919 27.5406 6.62528 24.8914 4.04943C22.2294 1.46119 17.9043 1.46119 15.2423 4.04943C14.8299 4.45042 14.1706 4.45042 13.7581 4.04943C11.0962 1.46119 6.77104 1.46119 4.10909 4.04943Z" fill="white")
 </template>
 
 <script>
@@ -19,4 +23,56 @@
 </script>
 
 <style lang="sass">
+  .random-breed
+    position: relative
+    overflow: hidden
+    display: grid
+    height: 290px
+    border-radius: 8px
+    z-index: 0
+    svg.vector-like
+      position: absolute
+      top: 25px
+      left: 25px
+      z-index: 100
+    img
+      display: block
+      min-height: 100%
+      width: 100%
+      position: absolute
+      top: 75%
+      left: 50%
+      transform: translate(-50%, -50%)
+    p.name
+      position: absolute
+      bottom: 25px
+      right: 25px
+      margin: 0
+      padding: 0
+      font-family: monospace
+      font-style: normal
+      font-weight: 600
+      font-size: 25px
+      line-height: 28px
+      /* identical to box height, or 112% */
+      letter-spacing: 0.01em
+      color: #FFFFFF
+      text-transform: capitalize
+      z-index: 100
+    div.resize
+      position: relative
+      width: 250px
+      height: 300px
+      resize: both
+      overflow: hidden
+      z-index: 10
+    div.inner
+      position: absolute
+      width: 100%
+      height: 100%
+      top: 0
+      left: 0
+      box-shadow: inset 0 -50px 85px rgba(0, 0, 0, .8)
+      overflow: hidden
+      z-index: 9
 </style>
